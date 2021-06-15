4469180971b69ff4d7a9342aea07b3e50c6d3e1e
SanjeevK007 <78865630+SanjeevK007@users.noreply.github.com>
2021-06-15 11:04:15 +0530

    removing php code

--- Load more.php
+++ Load more.php
@@ -13,28 +13,7 @@
 				  </ul>
 				</nav>
 			</header>	
-			<section id="load-more">
-				<center>
-				<?php
-					include "DbConnect.php";
-					$db = new DbConnect();
-					$conn = $db->connect();
-
-					$stmt = $conn->prepare("SELECT * FROM videos LIMIT 20");
-					$stmt->execute();
-					$videos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-					echo "<div class ='row'>";
-					foreach($videos as $video){
-						echo "<div class='col-md-6'>";
-						echo "<center><lable>".$video['title']."</lable></center>";
-						echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/'.$video['video_id'].'" frameborder="1"
-								allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-						echo "</div>";
-					}
-					echo "</div>";
-				?>
-				</center>
-			</section>
+			
 			<section id="Contact">
 				<div class="grid">
 					<h4>Contact : </h4>
