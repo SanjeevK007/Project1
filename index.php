b70e7a1f1b98fa7db3b9871beb93196cab4601ea
SanjeevK007 <78865630+SanjeevK007@users.noreply.github.com>
2021-06-21 12:20:29 +0530

    updating file

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 		<title>YouTrend | Gallery</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-		<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+		<link href="layout.css" rel="stylesheet" type="text/css" media="all">
 		<script src="layout/scripts/jquery.backtotop.js"></script>
 		<script src="layout/scripts/jquery.mobilemenu.js"></script>
 	</head>
