4e1d55ece956ccfe9531e62d003e921cbf8f1394
SanjeevK007 <78865630+SanjeevK007@users.noreply.github.com>
2021-06-21 12:10:04 +0530

    adding new layout files

--- Load more.php
+++ Load more.php
@@ -1,52 +1,78 @@
-<html>
-    <head>
-        <title>Load more Videos</title>
-        <link type="text/css" rel="stylesheet" href="style.css" >
-    </head>
-    <body>
-        <div class="container">
-			<header id="header">
-			<h1 id="title" class="text-center">Hurray !!Some More Videos. Enjoy!!</h1>
-				<nav id="nav-bar">
-				  <ul>
-					<li><a class="nav-link" href="index.php">Home</a></li>
-				  </ul>
-				</nav>
-			</header>	
-			<section id="load-more">
-				<center>
-				<?php
-					include "DbConnect.php";
-					$db = new DbConnect();
-					$conn = $db->connect();
-
-					$stmt = $conn->prepare("SELECT * FROM videos");
-					$stmt->execute();
-					$videos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-					echo "<div class ='row'>";
-					foreach($videos as $video){
-						echo "<div class='col-md-6'>";
-						echo "<center><lable>".$video['title']."</lable></center>";
-						echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/'.$video['video_id'].'" frameborder="1"
-								allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-						echo "</div>";
-					}
-					echo "</div>";
-				?>
-				</center>
-			</section>
-			
-			<section id="Contact">
-				<div class="grid">
-					<h4>Contact : </h4>
-					<p>
-						<center>Fell free to contact me @Sanjeevkumardev007@gmail.com</center>
-					</p>
+<html lang="en">
+	<head>
+		<title>YouTrend | Trending Videos</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+		<script src="layout/scripts/jquery.backtotop.js"></script>
+		<script src="layout/scripts/jquery.mobilemenu.js"></script>
+	</head>
+	<body id="top">
+		<div class="wrapper row1">
+			<header id="header" class="hoc clear">
+				<div id="logo" class="fl_left"> 
+				  <h1 class="logoname"><a href="index.php">You<span>T</span>rend</a></h1>
 				</div>
-			</section>
+				<nav id="mainav" class="fl_right"> 
+					<ul class="clear">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="#Contact">Contact</a></li>
+					</ul>
+				<form action="#">
+					<select>
+						<option selected="selected" value="">MENU</option>
+						<option value="index.php">Home</option>
+						<option value="#Contact">Contact</option>
+					</select>
+				</form>
+				</nav>
+			</header>
 		</div>
+		<div class="bgded overlay" style="background-image:url('layout/pics/01.jpg');">
+			<div id="breadcrumb" class="hoc clear"> 
+				<ul>
+					<li>Relax & Enjoy !!</li>
+				</ul>
+			</div>
+		</div>
+		
+		<div class="wrapper row3">
+			<main class="hoc container clear"> 
+				<div class="content"> 
+					<div id="gallery">
+							<center><header class="heading">YouTrend Gallery</header></center>
+							<?php	
+								include "DbConnect.php";
+								$db = new DbConnect();
+								$conn = $db->connect();
+								$stmt = $conn->prepare("SELECT * FROM videos LIMIT 25");
+								$stmt->execute();
+								$videos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+								foreach($videos as $video){
+									echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/'.$video['video_id'].'" frameborder="1"
+											allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+								}
+							?>
+					</div>
+					<nav class="pagination">
+						<button type="button"><a href ="Load more videos">Load more videos</a></button>
+					</nav>
+				</div>
+				<div class="clear"></div>
+		    </main>
+		</div>
+		<div id ="Contact" class="wrapper row5">
+			<div id="copyright" class="hoc clear">
+				<p><center>Fell free to contact me @Sanjeevkumardev007@gmail.com</center></p>
+				<p class="fl_left">Copyright © 2021 - All Rights Reserved</p>
+				<p class="fl_right">Designed by Sanjeev Kumar</p>
+			</div>
+		</div>
+		<div style ="position : fixed; bottom:10px;right: 10px; color: green;">
+			<strong>
+				By Sanjeev Kumar
+			</strong>
+		</div>
+		<a id="backtotop" href="#top" class="visible"><i class="fas fa-chevron-up"></i></a>
 	</body>
-<footer>
-	<span>Designed by Sanjeev Kumar</span>
-</footer>
 </html>
--- YoutubeVideos.php
+++ YoutubeVideos.php
@@ -1,53 +1,103 @@
-<html>
-<head>
-	<h1>Retrive Youtube Videos</h1>
-</head>
-<body>
-	<div class="container">
-		<header class="header">
-			<h1 id="title" class="text-center">Saving Youtube Videos into the Database</h1>
-		</header>
-	<?php
-		$key = "AIzaSyCZLkAh-QpVmnsw-Iqb4V7RzVOodcTrQ08";
-		$base_url = "https://www.googleapis.com/youtube/v3/";
-		$maxResult = 10;
-		$nextPageToken = "CAoQAA";
-		$apiError = 'Video not found';
-		try{
-			$API_URL = $base_url."search?order=date&part=snippet&maxResults=".$maxResult."&key=".$key."&pageToken=".$nextPageToken;
-			$apiData = @file_get_contents($base_url."search?order=date&part=snippet&maxResults=".$maxResult."&key=".$key."&pageToken=".$nextPageToken);
-			if($apiData){
-				$videos = json_decode( $apiData );
-			}
-			else{
-				throw new Exception('Invalid API key');
-			}		
-		}
-		catch(Exception $e){
-			$apiError = $e->getMassage();
-		}
-		include "DbConnect.php";
-		$db = new DbConnect();
-		$conn = $db->connect();
-
-		foreach($videos->items as $video){
-			$sql = "INSERT INTO `videos` (`id`, `title`, `video_id`, `description`, `video_url`, `video_murl`, `video_hurl`, `channel_id`, `channel_title`) 
-					VALUES (NULL, :title, :vid, :vdesc, :vurl, :vmurl, :vhurl, :cid, :ctitle)";
-
-			$stmt = $conn->prepare($sql);
-			$stmt->bindParam(":title",$video->snippet->title);
-			$stmt->bindParam(":vid",$video->id->videoId);
-			$stmt->bindParam(":vdesc",$video->snippet->description);
-			$stmt->bindParam(":vurl",$video->snippet->thumbnails->default->url);
-			$stmt->bindParam(":vmurl",$video->snippet->thumbnails->medium->url);
-			$stmt->bindParam(":vhurl",$video->snippet->thumbnails->high->url);
-			$stmt->bindParam(":cid",$video->snippet->channelId);
-			$stmt->bindParam(":ctitle",$video->snippet->channelTitle);
-			$stmt->execute();
-		}		
+<html lang="en">
+	<head>
+		<title>YouTrend | Save into Database</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+		<script src="layout/scripts/jquery.backtotop.js"></script>
+		<script src="layout/scripts/jquery.mobilemenu.js"></script>
+	</head>
+	<body id="top">
+		<div class="wrapper row1">
+			<header id="header" class="hoc clear">
+				<div id="logo" class="fl_left"> 
+				  <h1 class="logoname"><a href="index.php">You<span>T</span>rend</a></h1>
+				</div>
+				<nav id="mainav" class="fl_right"> 
+					<ul class="clear">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="#Contact">Contact</a></li>
+					</ul>
+				<form action="#">
+					<select>
+						<option selected="selected" value="">MENU</option>
+						<option value="index.php">Home</option>
+						<option value="#Contact">Contact</option>
+					</select>
+				</form>
+				</nav>
+			</header>
+		</div>
+		<div class="bgded overlay" style="background-image:url('layout/pics/01.jpg');">
+			<div id="breadcrumb" class="hoc clear"> 
+				<ul>
+					<li><a href="#">Home</a></li>
+				</ul>
+			</div>
+		</div>
 		
-	?>
-	</div>
-</body>
+		<div class="wrapper row3">
+			<main class="hoc container clear"> 
+				<div class="content"> 
+					<center><header class="heading">YouTrend Saving into database</header></center>
+					<p><center><strong>Trending YouTube Videos saved into Database</strong></center></p>
+					<?php
+						$key = "AIzaSyCZLkAh-QpVmnsw-Iqb4V7RzVOodcTrQ08";
+						$base_url = "https://www.googleapis.com/youtube/v3/";
+						$maxResult = 10;
+						$nextPageToken = "CAoQAA";
+						$apiError = 'Video not found';
+						try{
+							$API_URL = $base_url."search?order=date&part=snippet&maxResults=".$maxResult."&key=".$key."&pageToken=".$nextPageToken;
+							$apiData = @file_get_contents($base_url."search?order=date&part=snippet&maxResults=".$maxResult."&key=".$key."&pageToken=".$nextPageToken);
+							if($apiData){
+								$videos = json_decode( $apiData );
+							}
+							else{
+								throw new Exception('Invalid API key');
+							}		
+						}
+						catch(Exception $e){
+							$apiError = $e->getMassage();
+						}
+						include "DbConnect.php";
+						$db = new DbConnect();
+						$conn = $db->connect();
+
+						foreach($videos->items as $video){
+							$sql = "INSERT INTO `videos` (`id`, `title`, `video_id`, `description`, `video_url`, `video_murl`, `video_hurl`, `channel_id`, `channel_title`) 
+									VALUES (NULL, :title, :vid, :vdesc, :vurl, :vmurl, :vhurl, :cid, :ctitle)";
+
+							$stmt = $conn->prepare($sql);
+							$stmt->bindParam(":title",$video->snippet->title);
+							$stmt->bindParam(":vid",$video->id->videoId);
+							$stmt->bindParam(":vdesc",$video->snippet->description);
+							$stmt->bindParam(":vurl",$video->snippet->thumbnails->default->url);
+							$stmt->bindParam(":vmurl",$video->snippet->thumbnails->medium->url);
+							$stmt->bindParam(":vhurl",$video->snippet->thumbnails->high->url);
+							$stmt->bindParam(":cid",$video->snippet->channelId);
+							$stmt->bindParam(":ctitle",$video->snippet->channelTitle);
+							$stmt->execute();
+						}								
+					?>
+				</div>
+				<div class="clear"></div>
+		    </main>
+		</div>
+		<div style ="position : fixed; bottom:10px;right: 10px; color: green;">
+			<strong>
+				By Sanjeev Kumar
+			</strong>
+		</div>
+	
+		<div id ="Contact" class="wrapper row5">
+			<div id="copyright" class="hoc clear">
+				<p><center>Fell free to contact me @Sanjeevkumardev007@gmail.com</center></p>
+				<p class="fl_left">Copyright © 2021 - All Rights Reserved</p>
+				<p class="fl_right">Designed by Sanjeev Kumar</p>
+			</div>
+		</div>
+		<a id="backtotop" href="#top" class="visible"><i class="fas fa-chevron-up"></i></a>
+	</body>
 </html>
 
